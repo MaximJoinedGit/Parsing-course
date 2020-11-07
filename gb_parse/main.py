71062a6a3435d9db7d17b6c5e8c2eeb4b6c0bb24
Maxim <max_autoexotica@mail.ru>
2020-11-07 15:50:14 +0300

    Готово.

--- gb_parse/main.py
+++ gb_parse/main.py
@@ -1,14 +1,21 @@
+import os
+from dotenv import load_dotenv
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
 from gb_parse import settings
 from gb_parse.spiders.youla import YoulaSpider
 from gb_parse.spiders.hh import HhSpider
+from gb_parse.spiders.instagram import InstagramSpider
+
+load_dotenv('../.env')
 
 if __name__ == '__main__':
     crawl_settings = Settings()
     crawl_settings.setmodule(settings)
     crawl_proc = CrawlerProcess(settings=crawl_settings)
     # crawl_proc.crawl(YoulaSpider)
-    crawl_proc.crawl(HhSpider)
+    # crawl_proc.crawl(HhSpider)
+    crawl_proc.crawl(InstagramSpider, login=os.getenv('USERNAME'), enc_password=os.getenv('ENC_PASSWORD'))
     crawl_proc.start()
